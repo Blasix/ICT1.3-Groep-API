8cec7f2bb2321d72a8b344964b8020b42fe4ed03
BlasixMac <blasixcorp@gmail.com>
2025-04-01 17:27:15 +0200

    Add Note model

---
+++ LU1/Models/Note.cs
@@ -0,0 +1,10 @@
+namespace LU1.Models;
+
+public class Note
+{
+    public Guid Id { get; set; } // NVARCHAR(450)
+    public Guid ChildId { get; set; } // NVARCHAR(450)
+    public DateTime Date { get; set; } // DATETIME
+    public string Title { get; set; } // NVARCHAR(255)
+    public string Content { get; set; } // NVARCHAR(MAX)
+}
